a2a6bb383dc083b5ec91e0211dc9b17de158db6a
em-rosePetals <105890503+em-rosePetals@users.noreply.github.com>
2022-08-09 00:01:26 -0400

    Update letter.cpp

--- letter.cpp
+++ letter.cpp
@@ -1,13 +1,12 @@
-#include "Letter.h"
-
+/*
+ * letter.cpp
+ *
+ *  Created on: Aug 5, 2022
+ *      Author: emily
+ */
+#include "letter.hpp"
+#include <iostream>
 #include <cmath>
-
-
-struct SlopeHelper {
-	sf::Vector2f slope;
-	int vtcsNeeded;
-};
-
 /*
 * returns slope, non normalized
 */
@@ -21,12 +20,13 @@ sf::Vector2f nonNormSlope(const PtPair& pair) {
 */
 SlopeHelper normSlope(const PtPair& pair) {
 	sf::Vector2f slope = nonNormSlope(pair);
-	float normalizer = std::max(slope.x, slope.y);
+	float normalizer = -std::max(abs(slope.x), abs(slope.y));
 	slope /= normalizer;
-
+	SlopeHelper slopeHelp{slope, abs(static_cast<int>(normalizer))};
+	return slopeHelp;
 }
 /*
-* read the function name 
+* if required amount of vertices is greater than current length, resize
 */
 void resizeIfNeeded(sf::VertexArray& va, std::size_t start, std::size_t vtxCount) {
 	if (start + vtxCount > va.getVertexCount()) {
@@ -34,17 +34,19 @@ void resizeIfNeeded(sf::VertexArray& va, std::size_t start, std::size_t vtxCount
 	}
 }
 
-void drawLn(sf::VertexArray& va, std::size_t start, const PtPair& pair) {
+void drawLn(sf::VertexArray& va, std::size_t start, const Pt& offset, const PtPair& pair) {
 	SlopeHelper helper = normSlope(pair);
-
-	resizeIfNeeded(va, start, );
+	resizeIfNeeded(va, start, helper.verticesNeeded);
+	for (std::size_t i = start; i < va.getVertexCount(); ++i) {
+		Pt point = helper.slope * static_cast<float>(i) + offset;
+		va[i] = sf::Vertex(point, sf::Color::White);
+	}
 }
 
 
 /*
-/*
-  calculates the slope of a line between the two points
 
+  calculates the slope of a line between the two points
 float slope(const PtPair& pair) {
 	return slope(pair.first, pair.second);
 }
@@ -54,19 +56,16 @@ float slope(const Pt& first, const Pt& second) {
 	return rise / run;
 }
 
-/*
   calculates distance between the x coords of the
-
 float xDist(const PtPair& pair) {
 	return xDist(pair.first, pair.second);
 }
 float xDist(const Pt& first, const Pt& second) {
 	return first.x - second.x;
 }
-/*
+
   puts a sequence of points that represent a line between the points in the pair
   into the vertex array, starting at start
-
 void drawLn(sf::VertexArray& va, std::size_t start, const PtPair& pair) {
 	// slope of line
 	const float slp = slope(pair);
