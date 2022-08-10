0eaaf28d233e21c5da670232ff1f9a2d199cdc26
em-rosePetals <105890503+em-rosePetals@users.noreply.github.com>
2022-08-10 16:22:36 -0400

    Update letter.cpp

--- letter.cpp
+++ letter.cpp
@@ -1,12 +1,63 @@
-/*
- * letter.cpp
- *
- *  Created on: Aug 5, 2022
- *      Author: emily
- */
-#include "letter.hpp"
 #include <iostream>
-#include <cmath>
+#include <initializer_list>
+#include <cmath> // max, abs
+#include <memory>
+#include "letter.hpp"
+
+void drawLetter(sf::VertexArray& va, std::size_t start, const Letter& letter) {
+	for (std::size_t i = 0; i < letter.ptPairs.size(); ++i) {
+		drawLn(va, va.getVertexCount(), letter.origin, *letter.ptPairs[i]);
+	}
+}
+
+SlopeHelper normSlope(const PtPair& pair);
+void resizeIfNeeded(sf::VertexArray& va, std::size_t start, std::size_t vtxCount);
+/*
+* draws a line between the given points starting at the offset,
+and puts the component points into the vertex array starting at start
+*/
+void drawLn(sf::VertexArray& va, std::size_t start, const Pt& offset, const PtPair& pair) {
+	std::cout << "called drawLn: " << pair << '\n';
+	SlopeHelper helper = normSlope(pair);
+	std::cout << "norm slope: " << helper.slope << '\n';
+	std::cout << "before resize: " << va.getVertexCount();
+	resizeIfNeeded(va, start, helper.verticesNeeded);
+	std::cout << "after resize: " << va.getVertexCount() << '\n';
+	for (std::size_t i = start; i < va.getVertexCount(); ++i) {
+		Pt point = helper.slope * static_cast<float>(i) + offset;
+		va[i] = sf::Vertex(point, sf::Color::White);
+	}
+}
+
+Letter::Letter(const Pt& origin, const std::array<PtPair, 4>& pairs) {
+	this->origin = origin;
+	for (std::size_t i = 0; i < pairs.size(); ++i) {
+		this->ptPairs[i] = std::move(std::make_unique<PtPair>(pairs[i]));
+	}
+}
+Letter::~Letter() {
+	for (std::size_t i = 0; i < ptPairs.size(); ++i) {
+		ptPairs[i].reset();
+	}
+}
+
+std::ostream& operator<<(std::ostream& out, const Pt& point) {
+	out << "x: " << point.x << " y: " << point.y;
+	return out;
+}
+std::ostream& operator<<(std::ostream& out, const PtPair& pair) {
+	out << "first: " << pair.first << " second: " << pair.second;
+	return out;
+}
+std::ostream& operator<<(std::ostream& out, const Letter& letter) {
+	out << "origin:\n" << "    " << letter.origin << '\n';
+	out << "pairs:\n";
+	for (const std::unique_ptr<PtPair>& pair : letter.ptPairs) {
+		out << "    " << pair->first << " , " << pair->second << '\n';
+	}
+	return out;
+}
+
 /*
 * returns slope, non normalized
 */
@@ -22,7 +73,7 @@ SlopeHelper normSlope(const PtPair& pair) {
 	sf::Vector2f slope = nonNormSlope(pair);
 	float normalizer = -std::max(abs(slope.x), abs(slope.y));
 	slope /= normalizer;
-	SlopeHelper slopeHelp{slope, abs(static_cast<int>(normalizer))};
+	SlopeHelper slopeHelp{ slope, abs(static_cast<int>(normalizer)) };
 	return slopeHelp;
 }
 /*
@@ -33,49 +84,3 @@ void resizeIfNeeded(sf::VertexArray& va, std::size_t start, std::size_t vtxCount
 		va.resize(start + vtxCount);
 	}
 }
-
-void drawLn(sf::VertexArray& va, std::size_t start, const Pt& offset, const PtPair& pair) {
-	SlopeHelper helper = normSlope(pair);
-	resizeIfNeeded(va, start, helper.verticesNeeded);
-	for (std::size_t i = start; i < va.getVertexCount(); ++i) {
-		Pt point = helper.slope * static_cast<float>(i) + offset;
-		va[i] = sf::Vertex(point, sf::Color::White);
-	}
-}
-
-
-/*
-
-  calculates the slope of a line between the two points
-float slope(const PtPair& pair) {
-	return slope(pair.first, pair.second);
-}
-float slope(const Pt& first, const Pt& second) {
-	float rise = first.y - second.y;
-	float run = first.x - second.x;
-	return rise / run;
-}
-
-  calculates distance between the x coords of the
-float xDist(const PtPair& pair) {
-	return xDist(pair.first, pair.second);
-}
-float xDist(const Pt& first, const Pt& second) {
-	return first.x - second.x;
-}
-
-  puts a sequence of points that represent a line between the points in the pair
-  into the vertex array, starting at start
-void drawLn(sf::VertexArray& va, std::size_t start, const PtPair& pair) {
-	// slope of line
-	const float slp = slope(pair);
-	// number of vertices needed
-	const int vtxCount = static_cast<int>(-xDist(pair));
-	if (start + vtxCount > va.getVertexCount()) {
-		va.resize(start + vtxCount);
-	}
-	for (std::size_t i = start; i < va.getVertexCount(); ++i) {
-		va[i] = sf::Vertex(sf::Vector2f(1.0f * i, slp * i));
-	}
-}
-*/
